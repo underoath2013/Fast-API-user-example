8f839d2000207fa0e015165b7e5260407c95f174
user <user@gmail.com>
2023-01-29 02:18:28 +0300

    test yaml reading

--- main.py
+++ main.py
@@ -19,3 +19,16 @@ def save_openapi_json():
         json.dump(openapi_data, file)
 
 app.include_router(routes.router, prefix="/user", tags=["user"])
+
+
+import yaml
+import json
+
+with open('config.json', 'r') as file:
+    configuration = json.load(file)
+
+with open('config.yaml', 'w') as yaml_file:
+    yaml.dump(configuration, yaml_file)
+
+with open('config.yaml', 'r') as yaml_file:
+    print(yaml_file.read())
